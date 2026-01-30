1b2e1d0a8c55f09faca6caca8244c4d264c13728
srinivasvardhan12 <srinivasvardhan12@gmail.com>
2026-01-30 22:24:30 +0530

    Create 01_Unit.py

---
+++ Task1 04_IFCondition /01_Unit.py
@@ -0,0 +1,14 @@
+# 1. Program to determine the BMI Category based on user input.
+height = float(input("Enter height in meters: "))
+weight = float(input("Enter weight in kilograms: "))
+
+bmi = weight / (height ** 2)
+
+if bmi >= 30:
+    print("Obesity")
+elif 25 <= bmi < 30:
+    print("Overweight")
+elif 18.5 <= bmi < 25:
+    print("Normal")
+else:
+    print("Underweight")
